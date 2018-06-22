ac257b2555ca67c35ea225c3f5e3cab6e3d5f6d4
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-22 19:45:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,8 +4,10 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif "#{phrase}".upcase != true
     return "HUH?! SPEAK UP, SONNY!"
-  else
-    return "NO, NOT SINCE 1938!"
+  elsif "#{phrase}".upcase == true
+    return "NO, NOT SINCE 1938"
+  # else
+  #   return "NO, NOT SINCE 1938!"
 end
 end
 # Whatever you say to grandma, she should respond with
